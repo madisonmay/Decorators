e16394524a675a75ff1345519eb89b2fc48d8830
Madison May <madison.may@students.olin.edu>
2013-12-29 08:59:17 -0500

    Add support for methods in log.py

--- log.py
+++ log.py
@@ -1,13 +1,21 @@
+def hasmethod(obj, method_name):
+  return hasattr(obj, method_name) and callable(getattr(obj, method_name))
 
 def log(fn):
   def logger(*t, **d):
-    str_args = ", ".join(str(arg) for arg in t)
+    if hasmethod(t[0], fn.__name__):
+      args = t[1:]
+      prefix = t[0].__class__.__name__ + "." + fn.__name__
+    else:
+      args = t[:]
+      prefix = fn.__name__
+    str_args = ", ".join(str(arg) for arg in args)
     str_kwargs = ", ".join(str(k) + '=' + str(v) for k, v in d.items())
     join = ""
     if str_args and str_kwargs:
       join = ", "
     result = fn(*t, **d)
-    print fn.__name__ + "(" + str_args + join + str_kwargs + ') ->',
+    print prefix + "(" + str_args + join + str_kwargs + ') ->',
     print str(result)
     return result
   return logger
@@ -19,7 +27,15 @@ if __name__ == '__main__':
   def add(x, y):
     return x + y
 
+  class Adder:
+    @log
+    def add(self, x, y):
+      return x + y
+
   add(3, 5)
   add(1, y=2)
+
+  adder = Adder()
+  adder.add(1, 2)
 
 
